785e535064495ad355bf91129b63a96cf9cf8683
cuiqiu <cuiqiu.bupt@gmail.com>
2019-10-24 18:39:23 +0800

    *: add tidb cluster key metrics info table (#22)
    *: add tidb cluster key metrics info table

--- infoschema/inspection/const.go
+++ infoschema/inspection/const.go
@@ -25,6 +25,7 @@ var inspectionPersistTables = []string{
 	tablePersistTest,
 	tableTiDBClusterInfo,
 	tableSystemInfo,
+	tableTiDBClusterKeyMetrcisInfo,
 	tableTiDBKeyMetrcisInfo,
 	tableTiKVKeyMetrcisInfo,
 	tableTiDBPerformanceInfo,
@@ -53,7 +54,7 @@ const tableSystemInfo = `CREATE TABLE %s.SYSTEM_INFO (
   TYPE varchar(64) DEFAULT NULL,
   NAME varchar(64) DEFAULT NULL,
   IP varchar(64) DEFAULT NULL,
-  ADDRESS varchar(64) DEFAULT NULL,
+  STATUS_ADDRESS varchar(64) DEFAULT NULL,
   CPU varchar(64) DEFAULT NULL,
   CPU_USAGE varchar(64) DEFAULT NULL,
   MEMORY varchar(64) DEFAULT NULL,
@@ -64,19 +65,44 @@ const tableSystemInfo = `CREATE TABLE %s.SYSTEM_INFO (
   KERNAL varchar(128) DEFAULT NULL
 )`
 
+const tableTiDBClusterKeyMetrcisInfo = `CREATE TABLE %s.TIDB_CLUSTER_KEY_METRICS_INFO (
+  ID bigint(21) unsigned DEFAULT NULL,
+  CONNECTION_COUNT varchar(64) DEFAULT NULL,
+  QUERY_OK_COUNT varchar(64) DEFAULT NULL,
+  QUERY_ERR_COUNT varchar(64) DEFAULT NULL,
+  INSERT_COUNT varchar(64) DEFAULT NULL,
+  UPDATE_COUNT varchar(64) DEFAULT NULL,
+  DELETE_COUNT varchar(64) DEFAULT NULL,
+  REPLACE_COUNT varchar(64) DEFAULT NULL,
+  SELECT_COUNT varchar(64) DEFAULT NULL,
+  80_QUERY_DURATION varchar(64) DEFAULT NULL,
+  95_QUERY_DURATION varchar(64) DEFAULT NULL,
+  99_QUERY_DURATION varchar(64) DEFAULT NULL,
+  999_QUERY_DURATION varchar(64) DEFAULT NULL
+)`
+
 const tableTiDBKeyMetrcisInfo = `CREATE TABLE %s.TIDB_KEY_METRICS_INFO (
   ID bigint(21) unsigned DEFAULT NULL,
+  TYPE varchar(64) DEFAULT NULL,
   NAME varchar(64) DEFAULT NULL,
-  STATE varchar(64) DEFAULT NULL,
+  IP varchar(64) DEFAULT NULL,
+  STATUS_ADDRESS varchar(64) DEFAULT NULL,
   CONNECTION_COUNT bigint(21) unsigned DEFAULT NULL,
+  QUERY_OK_COUNT varchar(64) DEFAULT NULL,
+  QUERY_ERR_COUNT varchar(64) DEFAULT NULL,
+  80_QUERY_DURATION varchar(64) DEFAULT NULL,
+  95_QUERY_DURATION varchar(64) DEFAULT NULL,
+  99_QUERY_DURATION varchar(64) DEFAULT NULL,
+  999_QUERY_DURATION varchar(64) DEFAULT NULL,
   UPTIME varchar(64) DEFAULT NULL
 )`
 
 const tableTiKVKeyMetrcisInfo = `CREATE TABLE %s.TIKV_KEY_METRICS_INFO (
   ID bigint(21) unsigned DEFAULT NULL,
+  TYPE varchar(64) DEFAULT NULL,
   NAME varchar(64) DEFAULT NULL,
-  STATE varchar(64) DEFAULT NULL,
-  VERSION varchar(64) DEFAULT NULL,
+  IP varchar(64) DEFAULT NULL,
+  STATUS_ADDRESS varchar(64) DEFAULT NULL,
   CAPACITY varchar(64) DEFAULT NULL,
   AVAILABLE varchar(64) DEFAULT NULL,
   LEADER_COUNT bigint(21) unsigned DEFAULT NULL,
@@ -87,8 +113,6 @@ const tableTiKVKeyMetrcisInfo = `CREATE TABLE %s.TIKV_KEY_METRICS_INFO (
   REGION_WEIGHT double DEFAULT NULL,
   REGION_SCORE double DEFAULT NULL,
   REGION_SIZE bigint(21) unsigned DEFAULT NULL,
-  START_TS datetime DEFAULT NULL,
-  LAST_HEARTBEAT_TS datetime DEFAULT NULL,
   UPTIME varchar(64) DEFAULT NULL
 )`
 
